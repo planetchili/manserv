aefbdff33a0ddb441f433294385d83964e9ae4a7
planetchili <planetchili@users.noreply.github.com>
2018-01-29 03:03:11 +0900

    mandb::loadgame written

--- MancalaDatabase.php
+++ MancalaDatabase.php
@@ -10,7 +10,15 @@ class MancalaDatabase
 
     public function LoadGame( int $gameId ) : GameInfo 
     {
-        
+        $gameData = $this->conn->query( 'SELECT * FROM games WHERE id = '.$gameId );
+        assert( count( $gameData ) > 0,"LoadGame id not found" );
+        return new GameInfo( 
+            (int)$gameData['id'],
+            (int)$gameData['turn'],
+            (int)$gameData['player0Id'],
+            (int)$gameData['player1Id'],
+            new Side( (int)$gameData['activeSide'] )
+        );
     }
 }
 ?>
