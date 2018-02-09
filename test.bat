225deb3e605584d80d0bbf31ba379c1fb6e86001
planetchili <planetchili@users.noreply.github.com>
2018-02-09 12:36:07 +0900

    php test batching

--- test.bat
+++ test.bat
@@ -1,17 +1,2 @@
 @echo off
-
-setlocal
-
-set arg=%1
-set init=%arg:~0,1%
-if not !%arg% == ! (
-	if %init% == g (
-		vendor\bin\phpunit.bat --configuration test\phpunit.xml --group %arg%		
-	) else (
-		vendor\bin\phpunit.bat --configuration test\phpunit.xml --filter %arg%
-	)
-) else (
-	vendor\bin\phpunit.bat --configuration test\phpunit.xml
-)
-
-endlocal
+php -f "bin\testshell.php" -- %*
